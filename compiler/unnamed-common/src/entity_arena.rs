c065d1ffe358b0326ea82adfaffb62fd4cae7acd
Aern-do <radeonchik.new@gmail.com>
2025-09-05 09:43:08 +0300

    feat(common): implement index for entity arena

--- compiler/unnamed-common/src/entity_arena.rs
+++ compiler/unnamed-common/src/entity_arena.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use cranelift_entity::{EntityRef, ListPool, PrimaryMap, packed_option::ReservedValue};
 
 #[derive(Debug, Clone)]
@@ -18,5 +20,13 @@ impl<E: EntityRef + ReservedValue, T> Default for EntityArena<E, T> {
 impl<E: EntityRef + ReservedValue, T> EntityArena<E, T> {
     pub fn new() -> Self {
         Self::default()
+    }
+}
+
+impl<E: EntityRef + ReservedValue, T> Index<E> for EntityArena<E, T> {
+    type Output = T;
+
+    fn index(&self, index: E) -> &Self::Output {
+        &self.map[index]
     }
 }
